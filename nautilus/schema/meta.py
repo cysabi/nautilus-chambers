826923afc060be6db29e2378928607b19c0412d4
LeptoSpira <leptospira489@gmail.com>
2019-08-20 15:18:30 -0400

    Add meta type
    including resolvers

---
+++ nautilus/schema/meta.py
@@ -0,0 +1,19 @@
+from ariadne import ObjectType
+from nautilus import dbh
+
+meta_type = ObjectType("Meta")
+
+
+@meta_type.field("id")
+def resolve_id(obj, info):
+    return obj.id
+
+
+@meta_type.field("discord")
+def resolve_discord(obj, info):
+    return 571494333090496514
+
+
+@meta_type.field("twitter")
+def resolve_twitter(obj, info):
+    return None
