8bc34926a94b58eda695342bdaa1716fce908412
LeptoSpira <leptospira489@gmail.com>
2019-08-20 15:18:58 -0400

    Add wear type
    including resolvers

---
+++ nautilus/schema/wear.py
@@ -0,0 +1,14 @@
+from ariadne import ObjectType
+from nautilus import dbh
+
+wear_type = ObjectType("Wear")
+
+
+@wear_type.field("name")
+def resolve_name(obj, info):
+    return "TestWear"
+
+
+@wear_type.field("abilities")
+def resolve_abilities(obj, info):
+    return
