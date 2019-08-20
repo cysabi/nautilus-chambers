a51bab436ba83c84a14965797fe6227596e80bcc
LeptoSpira <leptospira489@gmail.com>
2019-08-20 14:51:52 -0400

    Add database handler

--- nautilus/__init__.py
+++ nautilus/__init__.py
@@ -1,5 +1,6 @@
 from ariadne import ObjectType, make_executable_schema, load_schema_from_path
 from flask import Flask
+import pymongo
 
 # Araidne
 types = load_schema_from_path("nautilus/schema.gql")
@@ -14,3 +15,15 @@ schema = make_executable_schema(types, [query, greeting])
 # Flask
 app = Flask(__name__)
 from nautilus import routes
+
+
+# PyMongo
+class DBHandler:
+
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.database = self.client["nautilus-chambers"]
+        self.collection = self.database["profiles"]
+
+
+dbh = DBHandler()
