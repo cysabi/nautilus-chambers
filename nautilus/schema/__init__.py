5934cdfc4808c29d4975b454a0bca32c6a97458a
LeptoSpira <leptospira489@gmail.com>
2019-08-20 15:09:16 -0400

    Add schema subpackage to hold types and resolvers

---
+++ nautilus/schema/__init__.py
@@ -0,0 +1,24 @@
+from ariadne import load_schema_from_path, make_executable_schema
+
+from .abilities import abilities_type
+from .wear import wear_type
+from .rank import rank_type
+from .gear import gear_type
+from .meta import meta_type
+from .status import status_type
+from .profile import profile_type
+from .query import query_type
+
+bindables = [
+    abilities_type,
+    wear_type,
+    rank_type,
+    gear_type,
+    meta_type,
+    status_type,
+    profile_type,
+    query_type
+]  # yapf: disable
+
+types = load_schema_from_path("nautilus/schema/.graphql")
+schema = make_executable_schema(types, bindables)
