01e9d38aa0e9097b2f2b23ba3b610795ea1280c3
LeptoSpira <leptospira489@gmail.com>
2019-08-20 15:18:14 -0400

    Add gear type
    Including resolvers

---
+++ nautilus/schema/gear.py
@@ -0,0 +1,24 @@
+from ariadne import ObjectType
+from nautilus import dbh
+
+gear_type = ObjectType("Gear")
+
+
+@gear_type.field("weapon")
+def resolve_weapon(obj, info):
+    return "Kensa Tetra Dualies"
+
+
+@gear_type.field("head")
+def resolve_head(obj, info):
+    return
+
+
+@gear_type.field("clothes")
+def resolve_clothes(obj, info):
+    return
+
+
+@gear_type.field("shoes")
+def resolve_shoes(obj, info):
+    return
