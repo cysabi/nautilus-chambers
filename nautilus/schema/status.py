7a97ff678551fed4073510f0d6bf196a755e1c09
LeptoSpira <leptospira489@gmail.com>
2019-08-20 15:48:38 -0400

    add status type
    and resolvers

---
+++ nautilus/schema/status.py
@@ -0,0 +1,29 @@
+from ariadne import ObjectType
+from nautilus import dbh
+
+status_type = ObjectType("Status")
+
+
+@status_type.field("ign")
+def resolve_ign(obj, info):
+    return "LeptoSpira"
+
+
+@status_type.field("level")
+def resolve_level(obj, info):
+    return 104
+
+
+@status_type.field("rank")
+def resolve_rank(obj, info):
+    return
+
+
+@status_type.field("gear")
+def resolve_gear(obj, info):
+    return
+
+
+@status_type.field("loadouts")
+def resolve_loadouts(obj, info):
+    return None
