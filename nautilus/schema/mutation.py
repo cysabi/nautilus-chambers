65e817e4d4431e36d01a33198b93f93ba6e7bd25
LeptoSpira <leptospira489@gmail.com>
2019-08-28 18:59:16 -0400

    Remove purpose fail

--- nautilus/schema/mutation.py
+++ nautilus/schema/mutation.py
@@ -6,11 +6,4 @@ mutation_type = ObjectType("Mutation")
 
 @mutation_type.field("updateProfile")
 def resolve_update_profile(obj, info, id, input):
-    if input.get('fail'):
-        return {
-            'status': False,
-            'error': 'Error raised by user',
-            'profile': True
-        }
-    else:
-        return {'status': True, 'profile': {'_id': id}}
+    return {'status': True, 'profile': {'_id': id}}
