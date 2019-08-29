c51f9e74cd3e53d8e27e5b5cf5f4a53968f0fb22
LeptoSpira <leptospira489@gmail.com>
2019-08-29 14:10:02 -0400

    create createProfile Mutation field

--- nautilus/schema/mutation.py
+++ nautilus/schema/mutation.py
@@ -4,6 +4,11 @@ from nautilus import dbh
 mutation_type = ObjectType("Mutation")
 
 
+@mutation_type.field("createProfile")
+def resolve_update_profile(*_, **kwargs):
+    return dbh.create_profile(kwargs)
+
+
 @mutation_type.field("updateProfile")
 def resolve_update_profile(*_, **kwargs):
     return dbh.update_profile(kwargs)
