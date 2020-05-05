cc00257fee1c36a07d9ca4fc9714d23c69cbe589
LeptoFlare <leptoflare@gmail.com>
2020-05-05 02:54:19 -0400

    Update database structure

--- nautilus/utils/db.py
+++ nautilus/utils/db.py
@@ -11,51 +11,36 @@ class DatabaseHandler:
     """Custom database handler that works with mongodb."""
 
     def __init__(self):
+        # Client & Database
         self.client = MongoClient("mongodb://mongo:27017/")
         self.db = self.client["nautilus"]
+        # Collections
         self.profiles = self.db["profiles"]
+        self.abilitysets = self.db["abilities"]
 
     @staticmethod
     def empty_profile():
         """Create a new empty profile."""
         return {
-            "discord": None,
             "meta": {
                 "private": None
             },
             "status": {
-                "gear": {
-                    "clothes": {
-                        "abilities": {
-                            "main": None,
-                            "subs": [None, None, None]
-                        },
-                        "name": None
-                    },
-                    "head": {
-                        "abilities": {
-                            "main": None,
-                            "subs": [None, None, None]
-                        },
-                        "name": None
-                    },
-                    "shoes": {
-                        "abilities": {
-                            "main": None,
-                            "subs": [None, None, None]
-                        },
-                        "name": None
-                    },
-                    "weapon": None
-                },
                 "ign": None,
+                "sw": None,
                 "level": None,
                 "rank": {
-                    "cb": None,
-                    "rm": None,
-                    "sr": None,
                     "sz": None,
-                    "tc": None
+                    "tc": None,
+                    "rm": None,
+                    "cb": None,
+                    "sr": None,
+                },
+                "gear": {
+                    "weapon": None,
+                    "head":    {"id": None, "abilities": None},
+                    "clothes": {"id": None, "abilities": None},
+                    "shoes":   {"id": None, "abilities": None}
                 }
             }
         }
