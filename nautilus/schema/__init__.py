7452e7aaf29d8bf07990773a318171fdfe2be15d
LeptoFlare <leptoflare@gmail.com>
2020-05-07 10:50:28 -0400

    Move data schema imports to init file

--- nautilus/schema/__init__.py
+++ nautilus/schema/__init__.py
@@ -1,6 +1,6 @@
 from .query import query_type
 from .mutation import mutation_type
-from .data.wear import wearhead_type, wearclothes_type, wearshoes_type
-from .data.abilities import abilities_type
+from .data import __all__
 
-bindables = [query_type, mutation_type, wearhead_type, wearclothes_type, wearshoes_type, abilities_type]
+
+bindables = [query_type, mutation_type, *__all__]
---
+++ nautilus/schema/data/__init__.py
@@ -0,0 +1,5 @@
+from .abilities import abilities_type
+from .wear import wearhead_type, wearclothes_type, wearshoes_type
+from .weapons import gear_type, weapon_type
+
+__all__ = [abilities_type, wearhead_type, wearclothes_type, wearshoes_type, gear_type, weapon_type]
