7c0d5b6d5ebcfd4b2088103432a5901b14221873
LeptoFlare <leptoflare@gmail.com>
2020-05-07 15:13:02 -0400

    swap "schema" and "graphql" folder

--- nautilus/graphql/mutation.py
+++ nautilus/graphql/mutation.py
@@ -8,8 +8,8 @@ mutation_type = MutationType()
 @mutation_type.field("createProfile")
 def resolve_create_profile(*_, discord, profile):
     """mutation createProfile"""
-    status, profile = utils.dbh.insert_profile(discord, profile)
     utils.logger.debug(f"createProfile | discord={discord}")
+    status, profile = utils.dbh.insert_profile(discord, profile)
     return {
         "status": status,
         "profile": profile,
@@ -20,10 +20,10 @@ def resolve_create_profile(*_, discord, profile):
 @mutation_type.field("updateProfile")
 def resolve_update_profile(*_, discord, profile):
     """mutation updateProfile"""
-    status, profile = utils.dbh.update_profile(discord, profile)
     utils.logger.debug(f"updateProfile | discord={discord}")
+    status, profile = utils.dbh.update_profile(discord, profile)
     return {
-        "status": True,
+        "status": status,
         "profile": profile,
         "error": None,
     }
@@ -32,10 +32,10 @@ def resolve_update_profile(*_, discord, profile):
 @mutation_type.field("deleteProfile")
 def resolve_delete_profile(*_, discord):
     """mutation deleteProfile"""
-    status, profile = utils.dbh.delete_profile(discord)
     utils.logger.debug(f"deleteProfile | discord={discord}")
+    status, profile = utils.dbh.delete_profile(discord)
     return {
-        "status": True,
+        "status": status,
         "profile": profile,
         "error": None,
     }
--- nautilus/graphql/query.py
+++ nautilus/graphql/query.py
@@ -8,10 +8,11 @@ query_type = QueryType()
 @query_type.field("readProfile")
 def resolve_read_profile(*_, discord):
     """query readProfile"""
-    profile = utils.dbh.find_profile(discord)
     utils.logger.debug(f"readProfile | discord={discord}")
+    profile = utils.dbh.find_profile(discord)
+    error = None if profile else "Unable to access profile. Either the profile doesn't exist or you do not have access to it."
     return {
         "status": profile is not None,
         "profile": profile,
-        "error": None,
+        "error": error,
     }
