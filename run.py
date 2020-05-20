a37c04cab3ecc793c58b54c0b9c3563bd255ebd5
LeptoFlare <leptoflare@gmail.com>
2020-05-20 01:38:41 +0000

    Polish run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 """Run nautilus."""
 from nautilus import app, utils
 
+
 if __name__ == "__main__":
-    app.run(debug=utils.env.get('debug'), host='0.0.0.0')
+    app.run(
+        host='0.0.0.0',
+        debug=utils.env.get('debug')
+    )
