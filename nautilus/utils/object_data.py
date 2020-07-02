c17d49a019ba9b5a1152670837e47dcb9a713a90
LeptoFlare <leptoflare@gmail.com>
2020-07-02 17:18:58 -0400

    Update object_data to use local dumps

--- nautilus/utils/object_data.py
+++ nautilus/utils/object_data.py
@@ -1,10 +1,11 @@
 """Functions that return json documents containing data of all objects of the particular object type."""
-import requests
+import json
 
 
 def get_object_by_id(obj: str, id, weapon_id=None):
     """Return the object from the json document with the corresponding id."""
-    json_data = requests.get(f'https://lepto.tech/nautilus-chambers/dumps/loadout-ink/{obj}.json').json()
+    with open(f'docs/dumps/loadout-ink/{obj}.json') as f:
+        json_data = json.load(f)
     try:
         data = next(filter(lambda i: i["id"] == id, json_data))
         if weapon_id is not None:
@@ -17,7 +18,8 @@ def get_object_by_id(obj: str, id, weapon_id=None):
 
 def get_rank(name: str = None, value: int = None):
     """Return the rank name or power from the json document with the corresponding id."""
-    json_data = requests.get(f'https://lepto.tech/nautilus-chambers/dumps/rank-powers/jp.json').json()
+    with open(f'docs/dumps/rank-powers/jp.json') as f:
+        json_data = json.load(f)
     for n, v in json_data:
         if n == name:
             return v
